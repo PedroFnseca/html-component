87ffd0bdeb51682f1c4d805add437c469a6f522c
pedrofnseca <pedropereira.pp264@gmail.com>
2025-05-04 20:22:10 -0300

    feat: add header, footer, and sidebar components with styles and loaders

---
+++ scripts/loaders/sidebar.js
@@ -0,0 +1,15 @@
+import loader from './index.js'
+
+function loadSidebar() {
+  const url = '/components/sidebar.html'
+
+  loader(url, function (response) {
+    const sidebar = document.querySelector('.sidebar')
+    
+    if (sidebar) {
+      sidebar.innerHTML = response
+    }
+  })
+}
+
+loadSidebar()
